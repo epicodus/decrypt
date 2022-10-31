aa4efb63fc292df116e51f04840d926a09090b78
Michael Kaiser-Nyman <michael@epicodus.com>
2022-10-31 13:51:10 -0500

    Remove interactivity, export unencrypted lines

--- decrypt.rb
+++ decrypt.rb
@@ -1,31 +1,27 @@
 require 'openssl'
 require 'base64'
 
-puts 'What is the name of the private key file?'
-private_key_file = gets.chomp
-
-puts 'What is the passphrase?'
-passphrase = gets.chomp
-
-puts 'What file do you want to import? Make sure it is a text file with each encrypted string on a new line.'
-import_filename = gets.chomp
-
+private_key_file = 'private.pem'
+passphrase = ''
+import_filename = 'input.txt'
 export_filename = 'export.txt'
 
 private_key = OpenSSL::PKey::RSA.new(File.read(private_key_file), passphrase)
 
 all_lines = []
 File.open(import_filename, 'r') do |file|
-  file.each_line do |encrypted_string|
-    all_lines << encrypted_string
+  file.each_line do |line|
+    all_lines << line
   end
 end
 
 File.open(export_filename, 'w') do |file|
-  all_lines.each do |encrypted_string|
-    # encrypted_string.gsub!(/\\n/, "\n")
-    ssn = private_key.private_decrypt(Base64.decode64(encrypted_string))
-    file.puts ssn
+  all_lines.each do |line|
+    begin
+      ssn = private_key.private_decrypt(Base64.decode64(line))
+      file.puts ssn
+    rescue
+      file.puts line
+    end
   end
 end
-puts "Decrypted content exported to #{export_filename}."
